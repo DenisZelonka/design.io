6e57746ca3ce8db6be6544a7cfa80853b7d78eab
Denis Zelonka <denis.zelonka@zurich.com>
2025-09-08 11:32:33 +0200

    Changed font to quicksand, changed intro to include summary, added button outlines, added button to scroll to top

---
+++ assets/js/to_top.js
@@ -0,0 +1,11 @@
+const toTopBtn = document.getElementById('toTopBtn');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        toTopBtn.classList.add('show');
+    } else {
+        toTopBtn.classList.remove('show');
+    }
+});
+toTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
